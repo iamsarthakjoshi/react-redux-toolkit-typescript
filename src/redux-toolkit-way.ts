66033a3709a0ccff6943ed13d566fcc893f46869
Sarthak Joshi <iamsarthakjoshi@gmail.com>
2020-07-23 23:32:59 +1000

    Code refactor

--- src/redux-toolkit-way.ts
+++ src/redux-toolkit-way.ts
@@ -24,7 +24,7 @@ const tasksSlice = createSlice({
   name: 'tasks',
   initialState,
   reducers: {
-    createTask: {
+    create: {
       reducer: (
         state,
         action: PayloadAction<{
@@ -45,7 +45,7 @@ const tasksSlice = createSlice({
         },
       }),
     },
-    editTask: (
+    edit: (
       state,
       action: PayloadAction<{ id: string; description: string }>
     ) => {
@@ -53,7 +53,7 @@ const tasksSlice = createSlice({
       const task = state.find((task) => task.id === payload.id);
       if (task) task.description = payload.description;
     },
-    toggleTask: (
+    toggle: (
       state,
       action: PayloadAction<{ id: string; isComplete: boolean }>
     ) => {
@@ -61,7 +61,7 @@ const tasksSlice = createSlice({
       const task = state.find((task) => task.id === payload.id);
       if (task) task.isComplete = payload.isComplete;
     },
-    deleteTask: (state, action: PayloadAction<{ id: string }>) => {
+    delete: (state, action: PayloadAction<{ id: string }>) => {
       const { payload } = action;
       const index = state.findIndex((task) => task.id === payload.id);
       if (index !== -1) state.splice(index, 1);
@@ -73,8 +73,7 @@ const selectedTaskSlice = createSlice({
   name: 'selectedTask',
   initialState: null as string | null,
   reducers: {
-    selectedTask: (state, action: PayloadAction<{ id: string }>) =>
-      action.payload.id,
+    select: (state, action: PayloadAction<{ id: string }>) => action.payload.id,
   },
 });
 
@@ -84,23 +83,21 @@ const counterSlice = createSlice({
   initialState: 0,
   reducers: {},
   extraReducers: {
-    [tasksSlice.actions.createTask.type]: (state) => state + 1,
-    [tasksSlice.actions.editTask.type]: (state) => state + 1,
-    [tasksSlice.actions.toggleTask.type]: (state) => state + 1,
-    [tasksSlice.actions.deleteTask.type]: (state) => state + 1,
+    [tasksSlice.actions.create.type]: (state) => state + 1,
+    [tasksSlice.actions.edit.type]: (state) => state + 1,
+    [tasksSlice.actions.toggle.type]: (state) => state + 1,
+    [tasksSlice.actions.delete.type]: (state) => state + 1,
   },
 });
 
 export const {
-  createTask: createTaskActionCreator,
-  deleteTask: deleteTaskActionCreator,
-  editTask: editTaskActionCreator,
-  toggleTask: toggleTaskActionCreator,
+  create: createTaskActionCreator,
+  delete: deleteTaskActionCreator,
+  edit: editTaskActionCreator,
+  toggle: toggleTaskActionCreator,
 } = tasksSlice.actions;
 
-export const {
-  selectedTask: selectTaskActionCreator,
-} = selectedTaskSlice.actions;
+export const { select: selectTaskActionCreator } = selectedTaskSlice.actions;
 
 const reducer = {
   tasks: tasksSlice.reducer,
